0aad969962a34be4d8f68c7527d980f62962d075
Lion981 <a.deerow98@gmail.com>
2023-12-21 17:54:19 +0000

    first commit

---
+++ src/StudentBlock.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const StudentBlock = () => {
+  return (
+    <div>
+      <h1>Students Beautiful class</h1>
+      <ul>
+        <li>
+          <p id="pid1">Mohamed</p>
+        </li>
+        <li>
+          <p id="pid2">Liibaan</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default StudentBlock;
